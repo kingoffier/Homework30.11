a68588dadd19131f8fde2c5810b9447a7e54fda5
kingoffier <144258616+kingoffier@users.noreply.github.com>
2023-11-30 17:08:23 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            int colvo = 0;
+            int sum = 0;
+            float arifm = 0;
+            int[] array = new int[n];
+            Console.WriteLine("Введите числа в массиве");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+
+            }
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+                Console.WriteLine(array[i]);
+            }
+            arifm = sum / array.Length;
+            Console.WriteLine("Числа, которые меньше ср. арифметического:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < arifm)
+                {
+                    Console.WriteLine(array[i]);
+                }
+            }
+        
+    }
+    }
+}
